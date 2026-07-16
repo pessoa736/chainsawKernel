3d2ea2d4e92c37f970d84f109230fbe0a3de186e
Davi Passos <pessoa736@github.io>
2026-07-16 00:22:30 -0300

    change: mudado a forma que o kernel interage com o tempo e video. alem de usar o kernel como objeto

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,85 +1,185 @@
 #![no_std]
 #![no_main]
+#![feature(generic_const_exprs)]
+#![feature(inherent_associated_types)]
+#![allow(incomplete_features)]
+#![feature(abi_x86_interrupt)]
 
-use limine::request::StackSizeRequest;
+use core::time::Duration;
+
+use embedded_time::duration::Seconds;
+use limine::request::{FramebufferRequest, StackSizeRequest};
 use limine::BaseRevision;
-use core::panic::PanicInfo;
+use spin::lazylock;
 
-use crate::video::{pack_color, swap_buffers};
+use crate::timer::{SubTimer, TimeInteration};
+use crate::{debuglog::dprint, timer::TIMER, video::{Vga, VirtualColor}};
 
+extern crate alloc;
+pub extern crate nalgebra as na;
+
+
+mod debuglog;
 mod memory;
+mod pmm;
+//mod lua;
 mod video;
 mod db_font;
 mod io;
 mod math;
-mod terminal;
-mod tensors;
+//mod terminal;
+//mod matriz;
+mod timer;
+//mod shell;
+mod stubs;
 
-
+#[no_mangle]
 #[used]
 #[link_section = ".requests"]
-static STACK_REQUEST: StackSizeRequest = StackSizeRequest::new(2 * 1024 * 1024);
+static STACK_REQUEST: StackSizeRequest = StackSizeRequest::new(32 * 1024 * 1024);
 
+#[no_mangle]
 #[used]
 #[link_section = ".requests"]
 static BASE_REVISION: BaseRevision = BaseRevision::new();
 
 
-pub fn init_kernel() 
-{
-    memory::init();
-    video::init();
-    io::init();   
-    terminal::init();
+#[used]
+#[link_section = ".requests"]
+static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
+
+
+#[repr(align(16))]
+struct Kernel {
+    timer: TIMER,
+    vga: Vga,
 }
 
 
-fn intro(){ 
-    let wid = video::get_width();
-    let hei = video::get_height();
-    
-    let mut t: f64 = 0.0;
-    unsafe 
+impl Kernel {
+    pub fn init(&mut self) 
     {
-        video::clear_screen(video::pack_color(240, 10, 10, 255));
-        for y in 0 .. (hei/2) 
-        {
-            for x in 0 .. (wid/2) 
-            {
-                video::set_pixel(x + wid/4, y+hei/4, video::lerp_color(pack_color(0, 0, 0, 0), 0xfff8f7c7, t as u8));
-            }
+        if !BASE_REVISION.is_supported() {panic!("NO_BASE\n")}
+        dprint(Some("BOOT"), "B");
+
+        io::init();
+        dprint(Some("IK.1"), "io inicializou");
+        
+        let (heap_phys_base, heap_size) = memory::init();
+        if let Some(memmap_response) = memory::MEMORY_MAP_REQUEST.response() {
+            pmm::init_with_response(memmap_response, heap_phys_base, heap_size);
+        } else {
+            panic!("Falha ao obter memmap para o PMM");
         }
+        dprint(Some("IK.2"), "Memoria inicializou");
+
+        self.vga.init(&FRAMEBUFFER_REQUEST);
+        dprint(Some("IK.3"), "video inicializou");
+
+        // dprint(Some("BOOT"), "C");
+    
+        // paint_welcome();
+        // dprint(Some("BOOT"), "D");
+        //dprint(, s);
+
+        // shell::init()
+    }
+
+    pub fn Loop(&mut self)
+    {
+        dprint(Some("loop"), "inicializou o loop");
+        loop {}
+    }
 
 
-        video::draw_string(wid/2, hei/2, "test aaaaaaaa", 0x000);
-        video::draw_string(wid/2, hei/2 + 8, "TEST 💖", 0x000);
+    pub fn paint_welcome(&mut self)
+    {
+        let mut wel_time = TimeInteration::new(Some(Duration::from_secs(2).as_millis() as u64));
 
-        t+=0.05;
+
+        dprint(Some("welcome"), "inicializou o welcome");
+
+        let wid = self.vga.get_width();
+        let hei = self.vga.get_height();
+
+        dprint(Some("welcome"), "pegou as proporções da tela");
+
+        self.vga.clear(VirtualColor::new(20, 20, 30, 255).unpack());
+        
+        dprint(Some("welcome"), "limpou a tela");
+
+        let bg_i_c = VirtualColor::new_hex(0x0);
+        let bg_f_c = VirtualColor::new_hex(0xcc2222ff);
+        let fg_i_c = VirtualColor::new_hex(0x0);
+        let fg_f_c = VirtualColor::new_hex(0xeeeeaaff);
+
+        wel_time.loop_(|_time | 
+        {
+            let bg = bg_i_c.lerp(bg_f_c, _time.percent());
+            let fg = fg_i_c.lerp(fg_f_c, _time.percent());
+
+            self.vga.clear(bg.unpack());
+            self.vga.draw_string(
+                wid/2 - 32 * 8, hei/2,
+                "BEM VINDO AO MOTOSSERA KERNEL", 
+                fg.unpack()
+            );
+            // Linhas decorativas
+            self.vga.draw_rect((wid/4, hei/3),(wid/2, 1), fg.unpack());
+            self.vga.draw_rect((wid/4, hei - hei/3), (wid/2, 1), fg.unpack());
+
+            self.vga.swap_buffers();
+        });
+        wel_time.reset();
+        wel_time.loop_(|_time | 
+        {
+            let bg = bg_f_c.lerp(bg_i_c, _time.percent());
+            let fg = fg_f_c.lerp(fg_i_c, _time.percent());
+
+            self.vga.clear(bg.unpack());
+            self.vga.draw_string(
+                wid/2 - 32 * 8, hei/2,
+                "BEM VINDO AO MOTOSSERA KERNEL", 
+                fg.unpack()
+            );
+            // Linhas decorativas
+            self.vga.draw_rect((wid/4, hei/3),(wid/2, 1), fg.unpack());
+            self.vga.draw_rect((wid/4, hei - hei/3), (wid/2, 1), fg.unpack());
+
+            self.vga.swap_buffers();
+        });
+
     }
 }
+
+unsafe impl Sync for Kernel {}
+
+
+/// Pinta uma tela única de boas-vindas no framebuffer. Chamada antes de
+/// entrar no shell, para garantir que algo renderiza (sem depender de
+/// loop de intro que nunca disparava o swap_buffers).
+
+static mut KERNEL: Kernel = Kernel {
+    timer: TIMER,
+    vga: Vga::new()
+};
 
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! 
+pub extern "C" fn _start() -> ()
 {
-    assert!(BASE_REVISION.is_supported());
 
-    init_kernel();
+    boot_marker();
 
-
-    terminal::write_str("hello world!\n\nwelcome to Motossera Kernel!\n\n");
     
-   
-
-    loop {
-        let value = io::get_keyboard_input();
-        if value != 0 {terminal::write_char(value);}
-        terminal::render();
-        swap_buffers();    
+    unsafe {
+        KERNEL.init();
+        KERNEL.paint_welcome();
+        KERNEL.Loop();
     }
 }
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+#[inline(never)]
+fn boot_marker() {
+    dprint(Some("BOOT"), "A" );
 }
